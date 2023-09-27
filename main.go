9234e6d173daf6c0615be16496cc3c25fbc3023a
Matt Tucker <matucker@microsoft.com>
2023-09-27 15:43:33 -0600

    add healthz and readyz probe endpoints

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"net/http"
 	"os"
 
 	"github.com/Azure/msi-acrpull/controllers"
 	"github.com/Azure/msi-acrpull/pkg/authorizer"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	msiacrpullv1beta1 "github.com/Azure/msi-acrpull/api/v1beta1"
 	// +kubebuilder:scaffold:imports
@@ -37,10 +42,12 @@ func init() {
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
+	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.Parse()
 	defaultACRServer := os.Getenv(defaultACRServerEnvKey)
 	defaultManagedIdentityResourceID := os.Getenv(defaultManagedIdentityResourceIDEnvKey)
@@ -49,16 +56,27 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "aks.azure.com",
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "aks.azure.com",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
+
+	if err := mgr.AddHealthzCheck("healthz", checkAPIServer(mgr)); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", checkAPIServer(mgr)); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
 	apbReconciler := &controllers.AcrPullBindingReconciler{
 		Client:                           mgr.GetClient(),
 		Log:                              ctrl.Log.WithName("controllers").WithName("AcrPullBinding"),
@@ -78,5 +96,16 @@ func main() {
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
+	}
+}
+
+func checkAPIServer(mgr manager.Manager) healthz.Checker {
+	return func(_ *http.Request) error {
+		// Try to list secrets to ensure API server is reachable
+		if err := mgr.GetClient().List(context.Background(), &corev1.SecretList{}); err != nil {
+			return err
+		}
+
+		return nil
 	}
 }
